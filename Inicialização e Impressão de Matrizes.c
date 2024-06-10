854a3c69b8d3c3a75f0e0918869c782a11eecc1a
VictorKaue <98410524+VictorKaue@users.noreply.github.com>
2024-06-10 15:50:31 -0300

    Create  Inicialização e Impressão de Matrizes.c

---
+++ Inicialização e Impressão de Matrizes.c
@@ -0,0 +1,20 @@
+//Escreva um programa que inicialize uma
+//matriz 3x3 com valores inteiros de 1 a 9 e, em
+//seguida, imprima a matriz.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int matriz[3][3] = {1,2,3,4,5,6,7,8,9};
+	int i,j;
+	
+	for(i=0; i<3;i++){
+		for(j=0; j<3;j++){
+			printf("| %d | ", matriz[i][j]);
+		}
+		puts("");
+	}
+		
+	return 0;
+}
